3915b4bedd9cb13422129e64517b66ad4642604a
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-03 12:09:40 +0200

    added useState and onChange handler function

--- 17-starting-project/17-starting-project/src/components/SimpleInput.js
+++ 17-starting-project/17-starting-project/src/components/SimpleInput.js
@@ -1,9 +1,17 @@
-const SimpleInput = (props) => {
+import {useState, useRef} from "react";
+
+export default function SimpleInput(props) {
+  const [enteredName, setEnteredName] = useState("");
+
+  function nameInputChangeHandler(event){
+    setEnteredName(event.target.value);
+  }
+
   return (
     <form>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
@@ -12,4 +20,3 @@ const SimpleInput = (props) => {
   );
 };
 
-export default SimpleInput;
