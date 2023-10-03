7f3cfdbad56cbd07a5c503d339a7a9ed0507e741
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-03 14:56:58 +0200

    added simple validation feedback

--- 17-starting-project/17-starting-project/src/components/SimpleInput.js
+++ 17-starting-project/17-starting-project/src/components/SimpleInput.js
@@ -2,6 +2,7 @@ import {useState} from "react";
 
 export default function SimpleInput(props) {
   const [enteredName, setEnteredName] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   function nameInputChangeHandler(event){
     setEnteredName(event.target.value);
@@ -11,18 +12,24 @@ export default function SimpleInput(props) {
     event.preventDefault();
 
     if(enteredName.trim() === ""){
+      setIsValid(false);
       return;
     }
+
+    setIsValid(true);
 
     console.log(enteredName);
     setEnteredName("");
   }
 
+  const nameInputClasses = isValid ? "form-control" : "form-control invalid";
+
   return (
     <form onSubmit={submitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+        {!isValid && <p className="error-text">Name can not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
