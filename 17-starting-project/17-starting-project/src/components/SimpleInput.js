158ca0462e5ca9d016298b15417b44f3bf723e9f
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-03 16:13:21 +0200

    fixed invalid output/logic

--- 17-starting-project/17-starting-project/src/components/SimpleInput.js
+++ 17-starting-project/17-starting-project/src/components/SimpleInput.js
@@ -1,17 +1,25 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function SimpleInput(props) {
   const [enteredName, setEnteredName] = useState("");
-  const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(false);
+  const [inputIsTouched, setInputIsTouched] = useState(false);
 
-  function nameInputChangeHandler(event){
+  useEffect(() => {
+    if(isValid){
+      console.log("Name is valid:)");
+    }
+  }, [isValid]);
+
+  function nameInputChangeHandler(event) {
     setEnteredName(event.target.value);
   }
 
-  function submitHandler(event){
+  function submitHandler(event) {
     event.preventDefault();
+    setInputIsTouched(true);
 
-    if(enteredName.trim() === ""){
+    if (enteredName.trim() === "") {
       setIsValid(false);
       return;
     }
@@ -22,19 +30,25 @@ export default function SimpleInput(props) {
     setEnteredName("");
   }
 
-  const nameInputClasses = isValid ? "form-control" : "form-control invalid";
+  const nameInputIsInvalid = !isValid && inputIsTouched;
+
+  const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control";
 
   return (
     <form onSubmit={submitHandler}>
       <div className={nameInputClasses}>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
-        {!isValid && <p className="error-text">Name can not be empty</p>}
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && <p className="error-text">Name can not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
   );
-};
-
+}
