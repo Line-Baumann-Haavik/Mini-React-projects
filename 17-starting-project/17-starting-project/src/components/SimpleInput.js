d6c96f2fe97bdb361b9cfaaedfae0580853826c2
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-04 10:05:25 +0200

    cleaned up code, fixed small mistake with submitted form

--- 17-starting-project/17-starting-project/src/components/SimpleInput.js
+++ 17-starting-project/17-starting-project/src/components/SimpleInput.js
@@ -1,45 +1,34 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function SimpleInput(props) {
   const [enteredName, setEnteredName] = useState("");
-  const [isValid, setIsValid] = useState(false);
   const [inputIsTouched, setInputIsTouched] = useState(false);
+  const isValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !isValid && inputIsTouched;
 
-  useEffect(() => {
-    if(isValid){
-      console.log("Name is valid:)");
-    }
-  }, [isValid]);
 
   function nameInputChangeHandler(event) {
     setEnteredName(event.target.value);
+
+    if (event.target.value.trim() !== "") {
+    }
   }
 
   function inputBlurHandler(event){
     setInputIsTouched(true);
-
-    if (enteredName.trim() === "") {
-      setIsValid(false);
-      return;
-    }
   }
 
   function submitHandler(event) {
     event.preventDefault();
     setInputIsTouched(true);
 
-    if (enteredName.trim() === "") {
-      setIsValid(false);
+    if (!isValid) {
       return;
     }
 
-    setIsValid(true);
-
-    console.log(enteredName);
     setEnteredName("");
+    setInputIsTouched(false);
   }
-
-  const nameInputIsInvalid = !isValid && inputIsTouched;
 
   const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control";
 
