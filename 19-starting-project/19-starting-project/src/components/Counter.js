d8f5b8fdc6debbc59d1120d5690dd458da212edc
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-09 12:28:42 +0200

    added, connected and styled buttons that change state

--- 19-starting-project/19-starting-project/src/components/Counter.js
+++ 19-starting-project/19-starting-project/src/components/Counter.js
@@ -1,14 +1,27 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
   const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
+
+  function incrementHandler(){
+    dispatch({type: "increment"});
+  }
+
+  function decrementHandler(){
+    dispatch({type: "decrement"});
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
