b14e3e00a99cfc66d727c3e38459e098fe8ece94
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-09 11:46:21 +0200

    Set up correct counter showing, and connected store

--- 19-starting-project/19-starting-project/src/store/redux-store.js
+++ 19-starting-project/19-starting-project/src/store/redux-store.js
@@ -1,19 +1,18 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
 
-function counterReducer(state = {counter: 0}, action){
-    if(action.type === "increment"){
-        return({
-            counter: state.counter + 1
-        });
-    }
-    if(action.type === "decrement"){
-        return({
-            counter: state.counter - 1
-        })
-    }
+function counterReducer(state = { counter: 0 }, action) {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 
-    return state.counter;
-
+  return state;
 }
 
 const store = createStore(counterReducer);
