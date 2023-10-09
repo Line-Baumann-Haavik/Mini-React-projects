e38d5ff9b3f92e414f7ac1151b0a98b4decffc3c
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-09 13:10:54 +0200

    added payload and third button, changed reducer function

--- 19-starting-project/19-starting-project/src/components/Counter.js
+++ 19-starting-project/19-starting-project/src/components/Counter.js
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 const Counter = () => {
   const counter = useSelector(state => state.counter);
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
 
   function incrementHandler(){
     dispatch({type: "increment"});
@@ -14,12 +13,21 @@ const Counter = () => {
     dispatch({type: "decrement"});
   }
 
+  function increaseHandler(){
+    dispatch({type: "increase", amount: 5});
+  }
+
+  function toggleCounterHandler(){
+
+  }
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
