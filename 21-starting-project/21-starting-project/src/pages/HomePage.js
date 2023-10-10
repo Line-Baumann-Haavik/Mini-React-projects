b8deae3940f101ff27f0c8f1d625eca4689a7939
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-10 09:42:39 +0200

    added link to products page

--- 21-starting-project/21-starting-project/src/pages/HomePage.js
+++ 21-starting-project/21-starting-project/src/pages/HomePage.js
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
 
-
-export default function HomePage(){
-    return(<h1>Home Page!</h1>)
+export default function HomePage() {
+  return (
+    <>
+      <h1>Home Page!</h1>
+      <p>Link to <Link to="/products">products page</Link>.</p>
+    </>
+  );
 }
