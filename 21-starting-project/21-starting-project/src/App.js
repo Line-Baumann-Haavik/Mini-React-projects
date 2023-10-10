9cecd0d6a8431f49504f3843bf068f2f9feb1a1c
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-10 08:42:34 +0200

    added products page and correct routing to new page

--- 21-starting-project/21-starting-project/src/App.js
+++ 21-starting-project/21-starting-project/src/App.js
@@ -1,9 +1,10 @@
 import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
 
 const router = createBrowserRouter([
   {path: "/", element: <HomePage /> },
-  {}
+  {path: "/products", element: <ProductsPage />}
 ]);
 
 
