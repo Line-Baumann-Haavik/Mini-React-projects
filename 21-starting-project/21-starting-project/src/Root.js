d44e38f3ecedad8df020bb157d4b1514e8f1cffa
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-10 12:45:16 +0200

    fixed some styling

--- 21-starting-project/21-starting-project/src/Root.js
+++ 21-starting-project/21-starting-project/src/Root.js
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "./components/MainNavigation";
-import styles from "./Root.module.css";
+
 
 export default function Root() {
   return (
     <>
       <MainNavigation />
-      <main className={styles.content}>
+      <main>
         <Outlet />
       </main>
     </>
