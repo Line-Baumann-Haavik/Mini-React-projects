02017be2614caf194504a412135dcdbd67f874c4
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-10 14:16:55 +0200

    added and set up  useNavigate button on Home page

--- 21-starting-project/21-starting-project/src/pages/HomePage.js
+++ 21-starting-project/21-starting-project/src/pages/HomePage.js
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+    const navigate = useNavigate();
+
+    function NavigationTest(){
+        navigate("/products");
+    }
   return (
     <>
       <h1>Home Page!</h1>
       <p>Link to <Link to="/products">products page</Link>.</p>
+      <p><button onClick={NavigationTest}>Navigate</button></p>
     </>
   );
 }
