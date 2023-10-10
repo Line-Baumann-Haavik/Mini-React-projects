6abad2691762e30d04e97c32ab88c7e155fb11cb
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-10-10 10:44:57 +0200

    added navbar and set upp proper linking/navigation, styling and changed to using root file

--- 21-starting-project/21-starting-project/src/App.js
+++ 21-starting-project/21-starting-project/src/App.js
@@ -1,14 +1,19 @@
-import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
+import Root from "./Root";
 
 const router = createBrowserRouter([
-  {path: "/", element: <HomePage /> },
-  {path: "/products", element: <ProductsPage />}
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
-
 
 export default function App() {
   return <RouterProvider router={router} />;
 }
-
