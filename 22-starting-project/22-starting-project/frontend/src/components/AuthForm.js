0e8367905ee94fa52ba54e0826d42298f7dca71b
Line Baumann Haavik <moonlightdream0@gmail.com>
2023-12-04 11:49:19 +0100

    added and implemented tokens

--- 22-starting-project/22-starting-project/frontend/src/components/AuthForm.js
+++ 22-starting-project/22-starting-project/frontend/src/components/AuthForm.js
@@ -1,8 +1,11 @@
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import { Form, Link, useActionData, useNavigation, useSearchParams } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
+  const data = useActionData();
+  const navigation = useNavigation();
+
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
 
@@ -10,6 +13,12 @@ function AuthForm() {
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((error) => (<li key={error}>{error}</li>))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -19,10 +28,10 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`} >
             {isLogin ? 'Create new user' : 'Login'}
           </Link>
-          <button>Save</button>
+          <button disabled={navigation.state === "submitting"}>{navigation.state === "submitting" ? "Loading..." : "Save"}</button>
         </div>
       </Form>
     </>
